4c755b93f97b05210259f4c86101d1d60dc287c8
Joao Brum <joaovictorbrumcosta@gmail.com>
2026-07-03 14:20:54 -0300

    feat final project

--- galeria/models.py
+++ galeria/models.py
@@ -1,11 +1,24 @@
+from datetime import datetime
 from django.db import models
 
+
 class Fotografia(models.Model):
+
+    OPCOES_DE_CATEGORIA = [
+        ("NEBULOSA","Nebulosa"),
+        ("ESTRELA","Estrela"),
+        ("GALÁXIA","Galáxia"),
+        ("PLANETA","Planeta")
+    ]
+
     nome = models.CharField(max_length=100,null=False,blank=False)
     legenda = models.CharField(max_length=150,null=False,blank=False)
+    categoria = models.CharField(max_length=150,choices=OPCOES_DE_CATEGORIA, default='')
     descricao = models.TextField(null=False,blank=False)
-    fotografia = models.CharField(max_length=100,null=False,blank=False)
+    fotografia = models.ImageField(upload_to='fotos/%Y/%M/%D/',blank=True)
+    publicado = models.BooleanField(default=False)
+    data_fotografia = models.DateField(default=datetime.now,blank=False)
 
     def __str__(self):
-        return f"fotografia: {self.nome}"
+        return self.nome
     
